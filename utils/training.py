1d81412a0228ea2fa3b9deb11c893566aa2a715e
Aleksandar Vracarevic <aleksandar.vracarevic@zuehlke.com>
2022-08-31 01:08:31 +0200

    Refactor training.py

--- utils/training.py
+++ utils/training.py
@@ -10,9 +10,11 @@ from utils.data_preparation import SCAML_Dataset
 import tensorflow_addons as tfa
 
 import time
+
+
 class SCA_Trainer():
-    #TODO: tie trainer to a single model => add as constructor parameter
-    #TODO: indicate if already trained
+    # TODO: tie trainer to a single model => add as constructor parameter
+    # TODO: indicate if already trained
     def __init__(self, log_root_path='./logs/', save_root_path='./models/') -> None:
         self.log_root_path = log_root_path
         self.save_root_path = save_root_path
@@ -43,8 +45,8 @@ class SCA_Trainer():
         @param tag Additional info regarding training details, default is none
         @save_dir Path to save model to, default is './models
         """
-        assert(validation_split == 0.0 or validation_data is None)
-        assert(not(validation_data is None and validation_split == 0.0))
+        assert (validation_split == 0.0 or validation_data is None)
+        assert (not (validation_data is None and validation_split == 0.0))
 
         _time = datetime.now().strftime("%Y-%d-%m_%H-%M")
 
@@ -54,21 +56,24 @@ class SCA_Trainer():
 
         if callbacks_list is None:
             callbacks_list = [
-                tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=4),
-                tf.keras.callbacks.EarlyStopping(monitor='accuracy', patience=5, baseline=0.0039),
-                tf.keras.callbacks.TensorBoard(log_dir=_log_dir, histogram_freq=1),
+                tf.keras.callbacks.EarlyStopping(
+                    monitor='val_loss', patience=4),
+                tf.keras.callbacks.EarlyStopping(
+                    monitor='accuracy', patience=5, baseline=0.0039),
+                tf.keras.callbacks.TensorBoard(
+                    log_dir=_log_dir, histogram_freq=1),
                 tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_filepath,
-                                                save_weights_only=True,
-                                                monitor='val_accuracy',
-                                                mode='max',
-                                                save_best_only=True)
+                                                   save_weights_only=True,
+                                                   monitor='val_accuracy',
+                                                   mode='max',
+                                                   save_best_only=True)
             ]
 
         train_dataset = dataset.get_profiling_dataset(attack_byte)
         X_train = train_dataset.X
         y_train = train_dataset.y
 
-        history = model.fit(X_train, 
+        history = model.fit(X_train,
                             y_train,
                             batch_size=batch_size,
                             epochs=epochs,
@@ -76,10 +81,11 @@ class SCA_Trainer():
                             validation_split=validation_split,
                             validation_data=validation_data,
                             callbacks=callbacks_list)
-        
-        save_path = (self.save_root_path + model.name + tag) if save_dir == '' else save_dir
+
+        save_path = (self.save_root_path + model.name +
+                     tag) if save_dir == '' else save_dir
         model.save(save_path)
-        
+
         self.training_history = history
 
     def get_model_history(self):
@@ -90,22 +96,27 @@ class SCA_Trainer():
     def plot_model_history(self):
         """! Plot training and validation accuracy and loss
         """
-        plt.figure(figsize=(20,10))
+        plt.figure(figsize=(20, 10))
 
         plt.subplot(121)
 
-        random_accuracy = [1/256] * len(self.training_history.history['val_accuracy'])
+        random_accuracy = [1/256] * \
+            len(self.training_history.history['val_accuracy'])
 
+        plt.title(f"Accuracy")
+        plt.ylabel("Number of epochs")
         plt.plot(self.training_history.history['val_accuracy'])
         plt.plot(self.training_history.history['accuracy'])
         plt.plot(random_accuracy)
-        plt.legend(['val_acc', 'train_acc', 'random_acc'],loc='best')
+        plt.legend(['val_acc', 'train_acc', 'random_acc'], loc='best')
 
         plt.subplot(122)
 
+        plt.title(f"Loss function")
+        plt.ylabel("Number of epochs")
         plt.plot(self.training_history.history['val_loss'])
         plt.plot(self.training_history.history['loss'])
-        plt.legend(['val_loss', 'training_loss'],loc='best')
+        plt.legend(['val_loss', 'training_loss'], loc='best')
 
         plt.show()
 
@@ -113,7 +124,7 @@ class SCA_Trainer():
         """! Evaluate model quality based on SCA relevant metrics.
         This is equivalent to attacking single key byte (of possibly many different keys).
         Accuracy and key rank metrics are calculated during evaluation.
-        
+
         @keys Matrix of keys
         @plaintexts Matrix of plaintexts
         @key_byte Key byte index to attack (0, 255)
@@ -128,7 +139,8 @@ class SCA_Trainer():
 
         with tqdm(total=keys_to_attack) as pbar:
             for key_index in range(keys_to_attack):
-                attack_dataset = dataset.get_attack_dataset(key_index, attack_byte, num_traces=traces_per_chunk)
+                attack_dataset = dataset.get_attack_dataset(
+                    key_index, attack_byte, num_traces=traces_per_chunk)
                 X_attack, y_attack, keys, plaintexts = attack_dataset.X, attack_dataset.y, attack_dataset.keys, attack_dataset.plaintexts
 
                 true_key_byte = keys[0][0]
@@ -142,7 +154,8 @@ class SCA_Trainer():
                 self.y_predicted_list.append(predicted_y_categorical)
                 self.y_true_list.append(true_y_categorical)
 
-                key_predicted_probabilities = self.get_key_probabilities(predictions, plaintexts, attack_byte)
+                key_predicted_probabilities = self.get_key_probabilities(
+                    predictions, plaintexts, attack_byte)
 
                 # Accumulate key predictions and classify
                 probs = np.zeros(256)
@@ -150,13 +163,15 @@ class SCA_Trainer():
                 for i, p in enumerate(key_predicted_probabilities):
                     probs += p
                     rankings = np.argsort(probs)[::-1]
-                    true_key_byte_rank = np.where(rankings == true_key_byte)[0][0]
+                    true_key_byte_rank = np.where(
+                        rankings == true_key_byte)[0][0]
 
                     ranks.append(true_key_byte_rank)
 
                 self.key_ranks.append(ranks)
 
-                pbar.set_description(f"True key byte: {true_key_byte}, predicted: {rankings[0]}")
+                pbar.set_description(
+                    f"True key byte: {true_key_byte}, predicted: {rankings[0]}")
                 pbar.update()
 
         # self.update_success_rate()
@@ -165,7 +180,8 @@ class SCA_Trainer():
             # Correct key predictions for num_traces
             _ranks_col = np.asarray(self.key_ranks)[:, num_traces]
             _num_correct = np.sum(_ranks_col == 0, axis=0)
-            self.stats_per_trace[num_traces] = (_num_correct, (_num_correct/keys_to_attack)*100)
+            self.stats_per_trace[num_traces] = (
+                _num_correct, (_num_correct/keys_to_attack)*100)
 
         self.plot_key_ranks(self.key_ranks)
         self.plot_confusion_matrix()
@@ -188,7 +204,7 @@ class SCA_Trainer():
                 sbox_inverse = aes.reverse_SBOX(j)
                 predicted_key = sbox_inverse ^ pt_test
                 key_probabilities[i][predicted_key] = prediction_value
-        
+
         return np.array(key_probabilities)
 
     def plot_confusion_matrix(self):
@@ -235,22 +251,24 @@ class SCA_Trainer():
         # Average model accuracy for predicting intermediate values
         num_keys = len(self.y_predicted_list)
         num_traces = len(self.y_predicted_list[0])
-        avg_accuracy = np.sum(np.asarray(self.y_true_list) == np.asarray(self.y_predicted_list)) / num_keys
+        avg_accuracy = np.sum(np.asarray(self.y_true_list)
+                              == np.asarray(self.y_predicted_list)) / num_keys
 
-        print(f"Model achieved average accuracy of {avg_accuracy}% on {num_keys} different keys ({num_traces} traces each)")
+        print(
+            f"Model achieved average accuracy of {avg_accuracy}% on {num_keys} different keys ({num_traces} traces each)")
 
         # Print maximum recovery accuracy with x traces
         max_accuracy = max(self.stats_per_trace.values())[1]
         min_traces = max(self.stats_per_trace, key=self.stats_per_trace.get)
-        print(f"Maximum key recovery success of {max_accuracy} achieved with {min_traces} traces")
+        print(
+            f"Maximum key recovery success of {max_accuracy} achieved with {min_traces} traces")
 
         plt.xlabel('Number of traces')
         plt.ylabel('%% of the successful key byte guesses')
 
         plt.plot([x[1] for x in self.stats_per_trace.values()])
 
-
-    def attack_full_key(self, models, trainers, dataset, key_index, traces_per_chunk = 256, verbose=1):
+    def attack_full_key(self, models, trainers, dataset, key_index, traces_per_chunk=256, verbose=1):
         """! Perform full key recovery.
 
         @param models List of pretrained models where models[0] was trained on the first key byte,
@@ -266,16 +284,18 @@ class SCA_Trainer():
 
         recovered_key = []
 
-        #self.get_key_length()
+        # self.get_key_length()
         with tqdm(total=16) as pbar:
             for attack_byte, model in enumerate(models):
-                attack_dataset = dataset.get_attack_dataset(key_index, attack_byte, num_traces=traces_per_chunk)
+                attack_dataset = dataset.get_attack_dataset(
+                    key_index, attack_byte, num_traces=traces_per_chunk)
                 X_attack, y_attack, keys, plaintexts = attack_dataset.X, attack_dataset.y, attack_dataset.keys, attack_dataset.plaintexts
-                true_key = keys[:, 0] # TODO: not necessary each time
+                true_key = keys[:, 0]  # TODO: not necessary each time
 
                 predictions = model.predict(X_attack, verbose=verbose)
 
-                key_predicted_probabilities = self.get_key_probabilities(predictions, plaintexts, attack_byte)
+                key_predicted_probabilities = self.get_key_probabilities(
+                    predictions, plaintexts, attack_byte)
 
                 # Accumulate key predictions and classify
                 probs = np.zeros(256)
@@ -286,5 +306,6 @@ class SCA_Trainer():
 
                 recovered_key.append(rankings[0])
 
-                pbar.set_description(f"True key: {true_key}, predicted: {recovered_key}")
+                pbar.set_description(
+                    f"True key: {true_key}, predicted: {recovered_key}")
                 pbar.update()
